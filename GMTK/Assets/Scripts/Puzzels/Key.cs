949d93751b63a9652a3308df60eb12c90decbc9f
Theis194 <53541469+Theis194@users.noreply.github.com>
2023-07-08 22:00:58 +0200

    Saved scene :anguished: :palm_tree:

--- GMTK/Assets/Scripts/Puzzels/Key.cs
+++ GMTK/Assets/Scripts/Puzzels/Key.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Key : PuzzleObject
 {
-
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.collider.tag == "Player") {
             isActive = true;
